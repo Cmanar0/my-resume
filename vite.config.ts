2036c562de1d5a8f5b16a3d248504b30f883623f
Marian Adamus <cmanar0@gmail.com>
2025-05-08 15:07:05 +0300

    Fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { splitVendorChunkPlugin } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
-    splitVendorChunkPlugin()
+    vue()
   ],
   build: {
     target: 'esnext',
